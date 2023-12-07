0f53297027c95f1559fed196df8dd7c037b72a37
Matthew Rayermann <marayerm@microsoft.com>
2023-12-06 17:34:02 -0800

    Add IExtensionCommandSchemaProvider which has returns no command schemas

---
+++ src/chat/commandSchema/noCommandsExtension.ts
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { ExtensionCommandParameters, ExtensionCommandSchema, ExtensionCommandSubmitParametersResponse, IExtensionCommandSchemaProvider } from "./commandSchema";
+
+export class NoCommandsExtension implements IExtensionCommandSchemaProvider {
+    public async getCommandSchemas(): Promise<ExtensionCommandSchema[]> {
+        return []
+    }
+
+    public async submitParameters(api: ExtensionCommandSchema, _parameters: ExtensionCommandParameters): Promise<ExtensionCommandSubmitParametersResponse> {
+        switch (api.command) {
+            default:
+                throw new Error(`Unknown command ${api.command}`);
+        }
+    }
+}
