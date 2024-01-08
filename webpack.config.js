b46f8b4ee8f90662cba66bc77ae711d7c8538ccb
Matthew Rayermann <marayerm@microsoft.com>
2024-01-08 15:43:21 -0800

    remove unused external

--- webpack.config.js
+++ webpack.config.js
@@ -28,19 +28,7 @@ let config = dev.getDefaultWebpackConfig({
         '../build/default/validation': 'commonjs ../build/default/validation',
         '../build/Release/bufferutil': 'commonjs ../build/Release/bufferutil',
         '../build/default/bufferutil': 'commonjs ../build/default/bufferutil',
-
-        // ./getCoreNodeModule.js (path from windowsProcessTree.ts) uses a dynamic require which can't be webpacked
-        './getCoreNodeModule': 'commonjs getCoreNodeModule',
     },
-    plugins: [
-        // Copy files to dist folder where the runtime can find them
-        new CopyWebpackPlugin({
-            patterns: [
-                // getCoreNodeModule.js -> dist/node_modules/getCoreNodeModule.js
-                { from: './out/src/utils/getCoreNodeModule.js', to: 'node_modules' }
-            ]
-        })
-    ]
 });
 
 if (DEBUG_WEBPACK) {
