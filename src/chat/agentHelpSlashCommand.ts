6ab732044fa5fe2ad4dd1ba8bd9b2f1a45967f5b
Matthew Rayermann <marayerm@microsoft.com>
2024-01-22 15:12:53 -0800

    Fixes

--- src/chat/agentHelpSlashCommand.ts
+++ src/chat/agentHelpSlashCommand.ts
@@ -3,9 +3,9 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { AgentRequest } from "./agent";
+import { type AgentRequest } from "./agent";
 import { agentName } from "./agentConsts";
-import { SlashCommand, SlashCommandHandlerResult, SlashCommandsOwner } from "./slashCommands";
+import { type SlashCommand, type SlashCommandHandlerResult, type SlashCommandsOwner } from "./slashCommands";
 
 export const agentHelpCommandName = "help";
 
@@ -21,6 +21,6 @@ export function getAgentHelpCommand(agentSlashCommandsOwner: SlashCommandsOwner)
 
 async function agentHelpHandler(agentSlashCommandsOwner: SlashCommandsOwner, request: AgentRequest): Promise<SlashCommandHandlerResult> {
     const slashCommandsMarkdown = agentSlashCommandsOwner.getSlashCommands().map(([name, config]) => `- \`/${name}\` - ${config.longDescription || config.shortDescription}`).join("\n");
-    request.progress.report({ content: `Hi! I can help you with learn about, and develop code for Azure. Feel free to ask or tell me anything and I'll do my best to help. Or, if you know what you'd like to do, you can use the following commands to ask me for help:\n\n${slashCommandsMarkdown}` });
+    request.progress.report({ content: `Hi! I can help you learn about and develop code for Azure. Feel free to ask or tell me anything and I'll do my best to help. Or, if you know what you'd like to do, you can use the following commands:\n\n${slashCommandsMarkdown}` });
     return { chatAgentResult: {}, followUp: [] };
 }
