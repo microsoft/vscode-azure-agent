49027831bb0330488491f5cd1b34e80f3b55f85f
Matthew Rayermann <marayerm@microsoft.com>
2024-01-23 12:07:17 -0800

    Make some changes to slashCommandFromWizardBasedExtensionCommand

--- src/chat/wizardBasedExtensionSchema/slashCommandFromWizardBaseExtensionCommand.ts
+++ src/chat/wizardBasedExtensionSchema/slashCommandFromWizardBaseExtensionCommand.ts
@@ -30,19 +30,19 @@ export function slashCommandFromWizardBasedExtensionCommand(command: WizardBased
 
                 if (Object.keys(pickedParameters).length > 0) {
                     markdownResponseLines.push(`I have determined the following information needed for **${command.displayName}** based on our conversation:`);
-                    markdownResponseLines.push(...Object.keys(pickedParameters).map((parameterName) => `- ${parameterName}: ${pickedParameters[parameterName]}`));
+                    markdownResponseLines.push(...Object.keys(pickedParameters).map((parameterName) => `- ${pickedParameters[parameterName].parameterTitle}: ${pickedParameters[parameterName].pickedValueLabel}`));
                     markdownResponseLines.push(`\nIf any of that information is incorrect, feel free to ask me to change it or start over.`);
                     markdownResponseLines.push(`\nOtherwise, you can go ahead and start with that by clicking the **${command.displayName}** button below.`);
-                    if (unfilfilledParameters.length > 0) {
+                    if (Object.keys(unfilfilledParameters).length > 0) {
                         markdownResponseLines.push(`\nYou can also provide me more information. I am at least interested in knowing:`);
-                        markdownResponseLines.push(...unfilfilledParameters.map((parameterName) => `- ${parameterName}`));
+                        markdownResponseLines.push(...Object.keys(unfilfilledParameters).map((parameterName) => `- ${unfilfilledParameters[parameterName].parameterTitle}: ${unfilfilledParameters[parameterName].parameterDescription}`));
                     }
                 } else {
                     markdownResponseLines.push(`\nI was not able to determine any of the information needed for **${command.displayName}** based on our conversation.`);
                     markdownResponseLines.push(`\nYou can go ahead and click the **${command.displayName}** button below to get started, or provide me with more information.`);
-                    if (unfilfilledParameters.length > 0) {
+                    if (Object.keys(unfilfilledParameters).length > 0) {
                         markdownResponseLines.push(`\nIf you'd like to provide me with more information. I am at least interested in knowing:`);
-                        markdownResponseLines.push(...unfilfilledParameters.map((parameterName) => `- ${parameterName}`));
+                        markdownResponseLines.push(...Object.keys(unfilfilledParameters).map((parameterName) => `- ${unfilfilledParameters[parameterName].parameterTitle}: ${unfilfilledParameters[parameterName].parameterDescription}`));
                     }
                 }
 
@@ -62,17 +62,17 @@ export function slashCommandFromWizardBasedExtensionCommand(command: WizardBased
 }
 
 async function pickQuickPickItem<T extends AgentQuickPickItem>(request: AgentRequest, items: T[] | Thenable<T[]>, options: AgentQuickPickOptions): Promise<T | undefined> {
-    const optionTitle = options.title || options.placeHolder;
     const resolvedItems = await Promise.resolve(items);
+    resolvedItems.forEach((item) => item.agentMetadata = item.agentMetadata || {});
+
     const systemPrompt = getPickQuickPickItemSystemPrompt1(resolvedItems, options);
     const maybeJsonCopilotResponse = await getResponseAsStringCopilotInteraction(systemPrompt, request);
-    const copilotPickedItemTitle = getStringFieldFromCopilotResponseMaybeWithStrJson(maybeJsonCopilotResponse, ["value", "parameter", "parameterValue", optionTitle || "value"]);
+    const copilotPickedItemTitle = getStringFieldFromCopilotResponseMaybeWithStrJson(maybeJsonCopilotResponse, ["value", "parameter", "parameterValue", options.agentMetadata.parameterName || "value"]);
     return copilotPickedItemTitle === undefined ? undefined : resolvedItems.find((i) => i.label === copilotPickedItemTitle);
 }
 
 function getPickQuickPickItemSystemPrompt1(items: AgentQuickPickItem[], options: AgentQuickPickOptions): string {
     const itemToString = (item: AgentQuickPickItem): string => `'${item.label}'${item.description ? ` (${item.description || ""})` : ""}`;
-    // join all the items into a string separted by commas
     const itemsString = items
         .filter((item) => item.kind !== vscode.QuickPickItemKind.Separator)
         .filter((item) => item.agentMetadata.notApplicableToAgentPick !== true)
@@ -87,15 +87,15 @@ function getPickQuickPickItemSystemPrompt1(items: AgentQuickPickItem[], options:
         .replace(/\)\)/g, ")");
 
     return [
-        `You are an expert in determining the value of a '${options.agentMetadata.paramterNameTitle}' parameter based on user input.`,
+        `You are an expert in determining the value of a '${options.agentMetadata.parameterName}' parameter based on user input.`,
         `The possible values for the parameter are: ${itemsString}.`,
-        `Given the user's input, your job is to determine a value for '${options.agentMetadata.paramterNameTitle}'.`,
+        `Given the user's input, your job is to determine a value for '${options.agentMetadata.parameterName}'.`,
         `Only repsond with a JSON summary (for example, '{value: "xyz"}') of the value you determine. Do not respond in a coverstaional tone, only JSON. If the users input does not infer or specify a value for this parameter, then do not respond.`,
     ].filter(s => !!s).join(" ");
 }
 
-type AzureAgentUserInputPickedParameters = { [parameterName: string]: string };
-type AzureAgentUserInputUnfilfilledParameters = string[];
+type AzureAgentUserInputPickedParameters = { [parameterName: string]: { parameterTitle: string, pickedValueLabel: string } };
+type AzureAgentUserInputUnfilfilledParameters = { [parameterName: string]: { parameterTitle: string, parameterDescription: string } };
 type AzureAgentUserInputResults = {
     pickedParameters: AzureAgentUserInputPickedParameters;
     unfilfilledParameters: AzureAgentUserInputUnfilfilledParameters;
@@ -112,7 +112,7 @@ class AgentAzureUserInput implements IAgentUserInput {
     constructor(request: AgentRequest) {
         this._request = request;
         this._pickedParameters = {};
-        this._unfilfilledParameters = [];
+        this._unfilfilledParameters = {};
         this._userInputReturnValueQueue = [];
         this._onDidFinishPromptEventEmitter = new vscode.EventEmitter<PromptResult>();
     }
@@ -122,21 +122,30 @@ class AgentAzureUserInput implements IAgentUserInput {
             throw new Error("canPickMany is not supported.");
         }
 
+        // Hack until Nathan or Alex go and add agentMetadata to wherever the heck subscription picks are defined
+        if (options.placeHolder?.indexOf("subscription") !== -1) {
+            options.agentMetadata = options.agentMetadata || {
+                paramterNameTitle: "Subscription",
+                parameterName: "subscription",
+                parameterDescription: "The subscription that the resource should be created in.",
+            }
+        }
+
         const parameterName = options.agentMetadata.parameterName;
         const pickedItem = await pickQuickPickItem(this._request, items, options);
         if (pickedItem !== undefined) {
-            this._pickedParameters[parameterName] = pickedItem.label;
+            this._pickedParameters[parameterName] = { parameterTitle: options.agentMetadata.paramterNameTitle, pickedValueLabel: pickedItem.label };
             this._userInputReturnValueQueue.push(pickedItem);
             this._onDidFinishPromptEventEmitter.fire({ value: pickedItem });
             return pickedItem;
         } else {
-            this._unfilfilledParameters.push(parameterName);
+            this._unfilfilledParameters[parameterName] = { parameterTitle: options.agentMetadata.paramterNameTitle, parameterDescription: options.agentMetadata.parameterDescription };
             throw new UserCancelledError(parameterName);
         }
     }
 
     public async showInputBox(options: AgentInputBoxOptions): Promise<string> {
-        this._unfilfilledParameters.push(options.agentMetadata.paramterNameTitle);
+        this._unfilfilledParameters[options.agentMetadata.parameterName] = { parameterTitle: options.agentMetadata.paramterNameTitle, parameterDescription: options.agentMetadata.parameterDescription };
         this._userInputReturnValueQueue.push(undefined);
         return "AGENTSKIPPING";
     }
