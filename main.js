ba03a86fafed47b7ea721b6750c2eb56c070d9e1
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2024-03-05 12:22:27 -0500

    Modernize webpacking

--- main.js
+++ main.js
@@ -16,13 +16,16 @@ let perfStats = {
 
 Object.defineProperty(exports, "__esModule", { value: true });
 
-const extension = require('./out/src/extension');
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
+const extension = require('./dist/extension.bundle');
 
 async function activate(ctx) {
-    return await extension.activateInternal(ctx, perfStats, true /* ignoreBundle */);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+    return await extension.activateInternal(ctx, perfStats);
 }
 
 async function deactivate(ctx) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     return await extension.deactivateInternal(ctx, perfStats);
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,94 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+// @ts-check // Check this file for typing issues (helps prevent mistakes in options passed)
+/* eslint-disable no-undef */ // Ignore the fact that the engine (which is webpack) is unknown
+
 'use strict';
 
-//@ts-check
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+/* eslint-enable @typescript-eslint/no-var-requires */
 
-// See https://github.com/Microsoft/vscode-azuretools/wiki/webpack for guidance
+const debugWebpack = !!process.env.DEBUG_WEBPACK;
 
-const process = require('process');
-const dev = require("@microsoft/vscode-azext-dev");
+/** @type {import('webpack').Configuration} */ // Here's where we can get typing help even though it's JS
+const config = {
+    target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+    mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+    cache: true, // Makes 'watch' builds way faster after the first full build
 
-let DEBUG_WEBPACK = !/^(false|0)?$/i.test(process.env.DEBUG_WEBPACK || '');
-
-let config = dev.getDefaultWebpackConfig({
-    target: 'node',
-    projectRoot: __dirname,
-    verbosity: DEBUG_WEBPACK ? 'debug' : 'normal',
-    externals:
-    {
-        // Fix "Module not found" errors in ./node_modules/websocket/lib/{BufferUtil,Validation}.js
-        // These files are not in node_modules and so will fail normally at runtime and instead use fallbacks.
-        // Make them as external so webpack doesn't try to process them, and they'll simply fail at runtime as before.
-        '../build/Release/validation': 'commonjs ../build/Release/validation',
-        '../build/default/validation': 'commonjs ../build/default/validation',
-        '../build/Release/bufferutil': 'commonjs ../build/Release/bufferutil',
-        '../build/default/bufferutil': 'commonjs ../build/default/bufferutil',
+    entry: {
+        /* eslint-disable @typescript-eslint/naming-convention */
+        './extension.bundle': './src/extension.ts',
+        /* eslint-enable @typescript-eslint/naming-convention */
+    }, // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+    output: {
+        // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        libraryTarget: 'commonjs2'
     },
-});
+    devtool: 'nosources-source-map',
+    externals: {
+        vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    },
+    resolve: {
+        // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                // Default TypeScript loader for .ts files
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.node$/,
+                loader: 'node-loader',
+            },
+        ]
+    },
+    plugins: [
+        // Copy some needed resource files from external sources
+        new CopyPlugin({
+            patterns: [
+                './node_modules/@microsoft/vscode-azext-azureutils/resources/**/*.svg',
+            ],
+        }),
+    ],
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    /* eslint-disable @typescript-eslint/naming-convention */
+                    // Keep class and function names so that stacks aren't useless and things like UserCancelledError work
+                    keep_classnames: true,
+                    keep_fnames: true,
+                    /* eslint-enable @typescript-eslint/naming-convention */
+                }
+            }),
+        ]
+    },
+    ignoreWarnings: [
+        // Suppress some webpack warnings caused by dependencies
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        (warning) => false, // No other warnings should be ignored
+    ],
+};
 
-if (DEBUG_WEBPACK) {
+if (debugWebpack) {
     console.log('Config:', config);
 }
 
 module.exports = config;
+
+/* eslint-enable no-undef */
