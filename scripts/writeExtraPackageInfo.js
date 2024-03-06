5679b1fa7ad68335ee844082d7fe1f2fe280377e
Matthew Rayermann <marayerm@microsoft.com>
2024-03-06 13:30:49 -0800

    Fix path

--- scripts/writeExtraPackageInfo.js
+++ scripts/writeExtraPackageInfo.js
@@ -9,7 +9,7 @@ const path = require("path");
 async function addConfigValuesToPackageJson() {
     if (!!process.env.VSCODE_AZURE_AGENT_OPENAI_CONFIG_ENDPOINT_CI && !!process.env.VSCODE_AZURE_AGENT_EXTENSION_IDENTITY_CONFIG_ENDPOINT_CI) {
         console.log("Writing extra package info to package.json");
-        const packageJsonPath = path.join(__dirname, "package.json");
+        const packageJsonPath = path.join(__dirname, "../package.json");
         const packageJsonObj = require(packageJsonPath);
         packageJsonObj.openAiConfigEndpoint = process.env.VSCODE_AZURE_AGENT_OPENAI_CONFIG_ENDPOINT_CI?.toString();
         packageJsonObj.extensionIdentityConfigEndpoint = process.env.VSCODE_AZURE_AGENT_EXTENSION_IDENTITY_CONFIG_ENDPOINT_CI?.toString();
