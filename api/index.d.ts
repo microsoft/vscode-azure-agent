c551ed5649b85f54e0db28a3c18a8c344879e5d2
Matthew Rayermann <marayerm@microsoft.com>
2024-03-11 14:41:09 -0700

    Index file

--- api/index.d.ts
+++ api/index.d.ts
@@ -2,3 +2,6 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+
+export * from "./AgentIntegration";
+export * from "./AzureAgentInput";
