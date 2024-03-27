22d4d4f2c3ee21ba2a8a8d2703a8c574690d6fa8
Matthew Rayermann <marayerm@microsoft.com>
2024-03-26 21:26:50 -0700

    Suggest things to do when setting resource context

--- src/chat/setResourceContextCommand.ts
+++ src/chat/setResourceContextCommand.ts
@@ -4,25 +4,37 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { type AgentRequest } from "./agent";
-import { verbatimCopilotInteraction } from "./copilotInteractions";
-import { type SlashCommand, type SlashCommandHandlerResult } from "./slashCommands";
+import { getResponseAsStringCopilotInteraction } from "./copilotInteractions";
+import { type SlashCommand, type SlashCommandHandlerResult, type SlashCommandsOwner } from "./slashCommands";
 
 export const setResourceContextCommandName = "setResourceContext";
 
-export const setResourceContextCommand: SlashCommand = [
-    setResourceContextCommandName,
-    {
-        shortDescription: `Establish the context that you wish to talk about a specific Azure resource.`,
-        longDescription: `Establish the context that you wish to talk about a specific Azure resource.`,
-        intentDescription: `This is best when the user says they want to talk about one of their Azure resources.`,
-        handler: (request) => setResourceContextHandler(request)
-    }
-];
+export function getSetResourceContextCommand(agentSlashCommandsOwner: SlashCommandsOwner): SlashCommand {
+    return [
+        setResourceContextCommandName,
+        {
+            shortDescription: `Establish the context that you wish to talk about a specific Azure resource.`,
+            longDescription: `Establish the context that you wish to talk about a specific Azure resource.`,
+            intentDescription: `This is best when the user says they want to talk about one of their Azure resources.`,
+            handler: (request) => setResourceContextHandler(agentSlashCommandsOwner, request)
+        }
+    ];
+}
 
-async function setResourceContextHandler(request: AgentRequest): Promise<SlashCommandHandlerResult> {
-    // @todo idea: have the agent give examples of things it can do with/for the resource?
+async function setResourceContextHandler(agentSlashCommandsOwner: SlashCommandsOwner, request: AgentRequest): Promise<SlashCommandHandlerResult> {
     const respondToSetResourceContextSystemPrompt = `The user is asking to talk about a specific Azure resource. Given the resource they are saying they want to talk about, let them know: you understand their request, that they are free to ask questions about the resource, and that they can ask for help doing things with the resource.`
-    await verbatimCopilotInteraction(respondToSetResourceContextSystemPrompt, request);
+    const introParagraph = await getResponseAsStringCopilotInteraction(respondToSetResourceContextSystemPrompt, request);
+
+    const slashCommands = await agentSlashCommandsOwner.getSlashCommands();
+    const slashCommandsList = Array.from(slashCommands.entries()).map((entry) => `${entry[0]} (${entry[1].intentDescription || entry[1].shortDescription})`).join(", ");
+    const suggestIdeasBasedOnResourceTypeSystemPrompt = `Here are a list of things you can do: ${slashCommandsList}. Given the type of resource the user is asking to talk about, suggest some things they can you ask for help with. Word the suggestions in natural language senteneces, where you tell the user what they can ask you to do. For example: "You can ask me to...". Do not use the name of things directly. Only suggest things specific to the type of resource. Do not suggest something that applies in a general sense. Format your response as a markdown list. Only respond with the markdown list. Do not include any other content in your response other than the list.`;
+    const suggestionsList = await getResponseAsStringCopilotInteraction(suggestIdeasBasedOnResourceTypeSystemPrompt, request);
+
+    request.responseStream.markdown(introParagraph || "Got it! Ask me whatever you'd like.");
+    if (suggestionsList !== undefined) {
+        request.responseStream.markdown("\n\nHere's some ideas for how I can help:\n");
+        request.responseStream.markdown(suggestionsList);
+    }
 
     return { chatAgentResult: {}, followUp: [] };
 }
